f7c07b99b327188cf93964d44b9bca85d5b7f279
Dhrumil Patel <dhrumilp15@gmail.com>
2020-09-20 03:43:09 -0400

    Correct Logic

--- src/components/logic.js
+++ src/components/logic.js
@@ -15,11 +15,9 @@ export function withprovider(gpuhardware, hours, provider, region, modelname) {
       carbon /= gpu["GFLOPS32/W"];
     }
   });
-  // `carbon`: The wattage of the model
   carbon *= hours; // Wh
   carbon /= 1000.0; // kWh
   let kilowatt = carbon;
-  // console.log("The kWh: " + carbon);
 
   sources.forEach((source) => {
     if (source.provider === provider && source.region === region) {
@@ -28,11 +26,10 @@ export function withprovider(gpuhardware, hours, provider, region, modelname) {
     }
   });
 
-  // console.log("final kg of CO2: " + carbon);
   return [kilowatt, carbon];
 }
 
-export function withee(gpuhardware, hours, ee, region, modelname) {
+export function withee(gpuhardware, hours, ee, modelname) {
   let carbon = 3600 * hours; // in units of kg of CO2
   models.forEach((model) => {
     if (model.name === modelname) {
@@ -40,20 +37,14 @@ export function withee(gpuhardware, hours, ee, region, modelname) {
     }
   });
   // `carbon`: # of GFLOPs that will occur in execution
-  // console.log("# of GFLOPs that will occur: " + carbon);
+  carbon /= ee;
 
-  gpus.forEach((gpu) => {
-    if (gpu.name === gpuhardware) {
-      // console.log("GFLOPS32/W: " + gpu["GFLOPS32/W"]);
-      carbon /= gpu["GFLOPS32/W"];
-    }
-  });
   // `carbon`: The wattage of the model
   carbon *= hours; // Wh
   carbon /= 1000.0; // kWh
   let kilowatt = carbon;
 
-  carbon *= ee;
+  carbon *= 8000; // default g of CO2 for 1 KWh
   carbon /= 1000.0;
-  // console.log("final kg of CO2: " + carbon);
-  return [kilowatt, carbon]};
+  return [kilowatt, carbon];
+}
