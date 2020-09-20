6b305e95f39a3a153b2bfa5ccceec7ae47a0e256
Dhrumil Patel <dhrumilp15@gmail.com>
2020-09-20 01:55:22 -0400

    Broken logic

--- src/components/logic.js
+++ src/components/logic.js
@@ -4,26 +4,33 @@ import sources from "../data/sources.json";
 import models from "../data/models.json";
 
 function training(gpuhardware, hours, provider, region, modelname) {
-  let carbon = 3600 * hours * hours; // in units of kg of CO2
+  let carbon = 3600 * hours; // total number of seconds
   models.forEach((model) => {
     if (model.name === modelname) {
       carbon *= model.flops;
     }
   });
+  // `carbon`: The number of floating point operations that will occur in execution
+  console.log("# of GFLOPS that will occur: " + carbon);
 
   gpus.forEach((gpu) => {
     if (gpu.name === gpuhardware) {
-      carbon *= 1.0 / gpu["GFLOPS32/W"];
+      carbon /= gpu["GFLOPS32/W"];
     }
   });
+  // `carbon`: The wattage of the model
+  carbon *= hours;
+  carbon /= 1000.0;
+  console.log("The kWh: " + carbon);
 
   sources.forEach((source) => {
     if (source.provider === provider && source.region === region) {
-      carbon *= source.impact / 1000.0;
+      carbon *= source.impact;
     }
   });
+  
   // (GFLOP / s) * (3600s / hr) * hrs * (W/GFLOPS32) * hrs * (kg CO2 / KWh)
-
+  console.log("final kg of CO2: " + carbon);
   return carbon;
 }
 
