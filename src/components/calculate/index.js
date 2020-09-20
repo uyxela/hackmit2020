5389ec2a317aae426da9074af0a2d20a646b1280
Dhrumil Patel <dhrumilp15@gmail.com>
2020-09-20 05:17:08 -0400

    sicko mode (#6)
    * Broken logic
    
    * Correct Logic
    
    * Updates

--- src/components/calculate/index.js
+++ src/components/calculate/index.js
@@ -1,40 +1,40 @@
 import React, { useState } from "react";
 import styled from "styled-components";
-import CalculateComponent from './calculateComponent';
+import CalculateComponent from "./calculateComponent";
 
 const CalculateContainer = styled.div`
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: center;
-    background-color: #83D86F;
-    min-height: 70vh;
-    padding: 10vh 0 10vh 0;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  background-color: #83d86f;
+  min-height: 70vh;
+  padding: 10vh 0 10vh 0;
 `;
 
 const ComponentContainer = styled.div`
-    width: 80%;
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
+  width: 80%;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
 `;
 
 const CalculateTitle = styled.h1`
-    color: white;
-    font-size: 4rem;
-    font-weight: bold;
-    margin-bottom: 20px;
+  color: white;
+  font-size: 4rem;
+  font-weight: bold;
+  margin-bottom: 20px;
 `;
 
 const CalculateDescription = styled.p`
-    color: white;
-    font-size: 1.75rem;
-    text-align: justify;
-    text-justify: auto;
+  color: white;
+  font-size: 1.75rem;
+  text-align: justify;
+  text-justify: auto;
 `;
 
 const CalculateA = styled.a`
-    color: white;
+  color: white;
 `;
 
 const Sub = styled.sub`
@@ -43,19 +43,32 @@ const Sub = styled.sub`
 `;
 
 function Calculate() {
-    const [step, setStep] = useState(1);
+  const [step, setStep] = useState(1);
 
-    return (
-        <CalculateContainer>
-            <ComponentContainer>
-                <CalculateTitle>Calculate Your Impact</CalculateTitle>
-                <CalculateDescription>Find the carbon impact of a machine learning model computation by indicating indicating relevant configuration options.</CalculateDescription>
-                <br />
-                <CalculateDescription>Data obtained from <CalculateA href="https://iq.opengenus.org/floating-point-operations-per-second-flops-of-machine-learning-models/">OpenGenus IQ</CalculateA> and <CalculateA href="https://github.com/mlco2/impact/tree/master/data">MLCO<Sub>2</Sub> Impact</CalculateA>.</CalculateDescription>
-                <CalculateComponent step={step} setStep={setStep} />
-            </ComponentContainer>
-        </CalculateContainer>
-    )
+  return (
+    <CalculateContainer>
+      <ComponentContainer>
+        <CalculateTitle>Calculate Your Impact</CalculateTitle>
+        <CalculateDescription>
+          Find the carbon impact of a machine learning model computation by
+          indicating indicating relevant configuration options.
+        </CalculateDescription>
+        <br />
+        <CalculateDescription>
+          Calculated using data from{" "}
+          <CalculateA href="https://iq.opengenus.org/floating-point-operations-per-second-flops-of-machine-learning-models/">
+            OpenGenus IQ
+          </CalculateA>{" "}
+          and{" "}
+          <CalculateA href="https://github.com/mlco2/impact/tree/master/data">
+            MLCO<Sub>2</Sub> Impact
+          </CalculateA>
+          .
+        </CalculateDescription>
+        <CalculateComponent step={step} setStep={setStep} />
+      </ComponentContainer>
+    </CalculateContainer>
+  );
 }
 
 export default Calculate;
